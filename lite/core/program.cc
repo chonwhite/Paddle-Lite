dff4eb0d8e433403d185129df65ea51ccc58df81
MyPandaShaoxiang <txg4794@163.com>
2019-08-28 09:46:53 +0000

    feat: run resnet50 work

--- lite/core/program.cc
+++ lite/core/program.cc
@@ -40,7 +40,7 @@ void RuntimeProgram::SaveOpInfosToProgram(cpp::ProgramDesc* desc) {
 
 void RuntimeProgram::Run() {
   for (auto& inst : instructions_) {
-    VLOG(4) << ">> Running kernel: " << inst.op()->op_info()->Repr()
+    LOG(INFO) << ">> Running kernel: " << inst.op()->op_info()->Repr()
             << " on Target " << TargetToStr(inst.kernel()->target());
 
     inst.Run();
