254957cc1b1dff9be59e5706b3d7afc5a0f0412f
hanbuhe <hanbuhe@baidu.com>
2019-09-06 10:53:29 +0800

    merged with develop

--- lite/backends/fpga/lite_tensor.h
+++ lite/backends/fpga/lite_tensor.h
@@ -120,6 +120,12 @@ class TensorLite {
 
   void set_lod(const LoD &lod) { lod_ = lod; }
 
+  PrecisionType precision() const { return precision_; }
+  void set_precision(PrecisionType precision) { precision_ = precision; }
+
+  bool persistable() const { return persistable_; }
+  void set_persistable(bool persistable) { persistable_ = persistable; }
+
   // T is the data type and R is the return type
   // For OpenCL, the return type can be cl::Buffer
   // and the data type can be float/int8_t.
@@ -142,6 +148,8 @@ class TensorLite {
 
   size_t memory_size() const { return zynq_tensor_->memorySize(); }
 
+  size_t offset() const { return offset_; }
+
   bool IsInitialized() const { return buffer_->data(); }
 
   // Other share data to this.
@@ -150,6 +158,9 @@ class TensorLite {
   void CopyDataFrom(const TensorLite &other);
 
   TargetType target() const { return target_; }
+
+  template <typename T>
+  TensorLite Slice(int64_t begin, int64_t end) const;
 
   zynqmp::Tensor *ZynqTensor() const { return zynq_tensor_; }
 
@@ -165,10 +176,20 @@ class TensorLite {
 
  private:
   TargetType target_{TargetType::kHost};
+
+  // precision_ and persistable_ are only used for persistable vars.
+  // If your tensor wants to be saved and loaded correctly, you must
+  // set values of precision_ and persistable_ after updating it.
+  // If your tensor is just a temp tensor, such as activations,
+  // you can ignore these two attributes.
+  PrecisionType precision_{PrecisionType::kUnk};
+  bool persistable_{false};
+
   DDimLite dims_;
   std::shared_ptr<Buffer> buffer_;
   LoD lod_;
   size_t memory_size_{};
+  size_t offset_{0};
 
   zynqmp::Tensor *zynq_tensor_ = new zynqmp::Tensor();
 
@@ -213,6 +234,20 @@ template <typename T, typename R>
 R *TensorLite::mutable_data(TargetType target) {
   target_ = target;
   return mutable_data<T>();
+}
+
+template <typename T>
+TensorLite TensorLite::Slice(int64_t begin, int64_t end) const {
+  int64_t base = numel() / dims_[0];
+
+  TensorLite dst;
+  dst.buffer_ = buffer_;
+  dst.target_ = target_;
+  auto dst_dims = dims_;
+  dst_dims[0] = end - begin;
+  dst.Resize(dst_dims);
+  dst.offset_ = offset_ + static_cast<size_t>(begin * base) * sizeof(T);
+  return dst;
 }
 
 template <typename TensorT>
